7d5d29d86c4440116034df2e6a481fd3e76a03bb
EC2 Default User <ec2-user@ip-172-31-45-140.eu-central-1.compute.internal>
2022-05-28 17:09:42 +0000

    Toggle new expenses form created

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -38,8 +38,7 @@ const ExpenseForm = (props) => {
     };
     
     return (
-        
-        <form onSubmit={submitHandler} >
+                <form onSubmit={submitHandler} >
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
@@ -54,13 +53,14 @@ const ExpenseForm = (props) => {
                     <input type="date" min="2019-01-01" max="2022-12-31" value={enteredDate} onChange={dateChangeHandler} />
                 </div>
             </div>
-            <div className="new-expense__actions">
-                <button type="submit">Add Expense</button>
+            <div className="new-expenses-actions-container">
+                <div className="new-expense__actions">
+                    <button type="button" onClick={props.onCancel}>Cancel</button>
+                    <button type="submit">Add Expense</button>
+                </div>
             </div>
         </form>
-        
         );
-    
 };
 
 export default ExpenseForm;
